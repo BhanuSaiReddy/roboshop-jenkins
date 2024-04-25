2fabf77d9afe9ba57c48b1f38ac0589ae5937c0f
bhanu <bhanusaireddy5244@gmail.com>
2024-04-25 18:07:14 +0530

    terraform.io

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -4,23 +4,23 @@ def call() {
         stage('Compile code') {
             common.compile
         }
-            print "hello"
-        }
+        print "hello"
+    }
 
-        stage('test') {
-            print "hello"
-        }
+    stage('test') {
+        print "hello"
+    }
 
-        stage('Compile Quality') {
-            print "hello"
-        }
+    stage('Compile Quality') {
+        print "hello"
+    }
 
-        stage('Code security') {
-            print "hello"
-        }
+    stage('Code security') {
+        print "hello"
+    }
 
-        stage('Release') {
-            print "hello"
-        }
-
-
+    stage('Release') {
+        echo "hello"
+    }
+}
+}
