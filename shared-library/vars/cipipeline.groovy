26a3acf57ef2def038c3ff9a387a332fa88c81a8
bhanu <bhanusaireddy5244@gmail.com>
2024-08-08 19:39:47 +0530

    terraform.io

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -3,21 +3,34 @@ def call() {
         stage('Cleanup') {
             sh "find . | sed -e '1d' | xargs rm -rf"
         }
-        stage('Checkout') {
-            git branch: "$BRANCH_NAME" , url: "https://github.com/BhanuSaiReddy/${component}"
+
+        // Setting the branch_name based on the TAG_NAME or BRANCH_NAME
+        if (env.TAG_NAME ==~ ".*") {
+            env.branch_name = "refs/tags/${env.TAG_NAME}"
+        } else {
+            env.branch_name = "${env.BRANCH_NAME}"
         }
+
+        stage('Checkout') {
+            git branch: "$BRANCH_NAME", url: "https://github.com/BhanuSaiReddy/${component}"
+        }
+
         stage('Compile Code') {
             common.compile()
         }
+
         stage('Test') {
             print 'hello'
         }
+
         stage('Code Quality') {
             print 'hello'
         }
+
         stage('Code Security') {
             print 'hello'
         }
+
         stage('Release') {
             print 'Hello'
         }
