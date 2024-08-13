51bcb7ca41f77ee6b27c4b5ecff2febe058836a7
bhanu <bhanusaireddy5244@gmail.com>
2024-08-13 17:04:59 +0530

    terraform.io

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -35,13 +35,13 @@ def codeQuality() {
 
 def codeSecurity() {
     stage('Code Security') {
-        env.sonaruser = sh (script: ' aws ssm get-parameter --name "sonarqube.user" --with-decryption --query "Parameter.Value" |xargs', returnStdout: true).trim()
-        env.sonarpass = sh (script: 'aws ssm get-parameter --name "sonarqube.pass" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+        env.sonaruser = sh(script: ' aws ssm get-parameter --name "sonarqube.user" --with-decryption --query "Parameter.Value" |xargs', returnStdout: true).trim()
+        env.sonarpass = sh(script: 'aws ssm get-parameter --name "sonarqube.pass" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
         wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]]) {
-        sh "sonar-scanner -Dsonar.host.url=http://172.31.15.74:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonar.pass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true"
+            sh "sonar-scanner -Dsonar.host.url=http://172.31.15.74:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonar.pass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true"
+        }
     }
 }
-
 def release() {
     stage('Release') {
         echo 'release'
