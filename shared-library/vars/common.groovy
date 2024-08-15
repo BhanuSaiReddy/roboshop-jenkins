e6f31ee5b4f51886a985c025cba0b87702d12325
bhanu <bhanusaireddy5244@gmail.com>
2024-08-15 15:30:15 +0530

    terraform.io

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -22,21 +22,24 @@ def test() {
 
 def codeQuality() {
     stage('Code Quality') {
-        echo 'codeQuality'
-    }
-}
-
-def codeSecurity() {
-    stage('Code Security') {
-        env.sonaruser = sh(script: ' aws ssm get-parameter --name "sonarqube.user" --with-decryption --query "Parameter.Value" |xargs', returnStdout: true).trim()
-        env.sonarpass = sh(script: 'aws ssm get-parameter --name "sonarqube.pass" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+        env.sonaruser = sh(script: 'aws ssm get-parameter --name "sonarqube.user" --with-decryption --query "Parameter.Value" | xargs', returnStdout: true).trim()
+        env.sonarpass = sh(script: 'aws ssm get-parameter --name "sonarqube.pass" --with-decryption --query "Parameter.Value" | xargs', returnStdout: true).trim()
         wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]]) {
-            if(env.codeType == "maven") {
+            if (env.codeType == "maven") {
                 sh 'sonar-scanner -Dsonar.host.url=http://172.31.15.74:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
             } else {
                 sh "sonar-scanner -Dsonar.host.url=http://172.31.15.74:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true"
             }
         }
+    }
+}
+
+def codeSecurity() {
+    stage('Code Security') {
+        echo 'Code Security'
+
+        // IN code security we will generally use SAST & SCA checks
+        // Your company uses Checkmarx SAST and Checkmarx SCA for this.
     }
 }
 
