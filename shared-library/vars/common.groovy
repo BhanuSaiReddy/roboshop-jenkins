7ecd2e18e553842fd4962dc39de5e82148b25c19
bhanu <bhanusaireddy5244@gmail.com>
2024-08-15 15:11:32 +0530

    terraform.io

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -32,7 +32,7 @@ def codeSecurity() {
         env.sonarpass = sh(script: 'aws ssm get-parameter --name "sonarqube.pass" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
         wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]]) {
             if(env.codeType == "maven") {
-                sh 'sonar-scanner -Dsonar.host.url=http://172.31.89.117:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+                sh 'sonar-scanner -Dsonar.host.url=http://172.31.15.74:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
             } else {
                 sh "sonar-scanner -Dsonar.host.url=http://172.31.15.74:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true"
             }
